e9152cddf2f870780eaf909b16f49a0f631b1ae9
Michael-Kochis <rhishisikk@gmail.com>
2021-04-21 14:40:09 -0500

    MVP completed, project ready for submission.

--- index.js
+++ index.js
@@ -196,10 +196,18 @@ class Airplane {
       - Its constructor calls the parent constructor passing to it what it needs.
       - The constructor should also initialize `gradClassName` and `favInstructor` properties on the instance.
       - ProjectManager instances have the following methods:
-          + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
+          + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standby times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
+ class ProjectManager extends Instructor {
+   constructor(props) {
+     super(props);
+     this.gradClassName = props.gradClassName;
+     this.favInstructor = props.favInstructor;
+   }
+
+   standUp(channel) {return `${this.name} announces to ${channel}, @channel standy times!`}
+   debugsCode(student, subject) { return `${this.name} debugs ${student.name}'s code on ${subject}`}
      
  }
   /*
